bc2421419c8ae3e6be064aa4113365679cf05076
gafredo <gafredo@gmail.com>
2022-07-27 17:36:39 -0300

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <img
+      src="https://i.gifer.com/WnES.gif"
+      alt="pokemon"
+      class="pokemon__image"
+    />
+    <h1 class="pokemon__data">
+      <span class="pokemon__number">6</span> -
+      <span class="pokemon__name">bulbasaur</span>
+    </h1>
+
+    <img src="./assets/pokedex.png" alt="pokedex" class="pokedex" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Oxanium:wght@300;400;500;600;700;800&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Oxanium', cursive;
+}
+
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: linear-gradient(to bottom, rgb(0, 153, 255), #fff);
+  min-height: 100vh;
+}
+
+main {
+  display: inline-block;
+  margin-top: 2%;
+  padding: 15px;
+  position: relative;
+}
+.pokedex {
+  width: 100%;
+  max-width: 425px;
+}
+
+.pokemon__image {
+  position: absolute;
+  bottom: 55%;
+  left: 50%;
+  transform: translate(-63%, 20%);
+  height: 18%;
+}
+
+.pokemon__data {
+  position: absolute;
+  font-weight: 600;
+  color: #aaa;
+  top: 54.5%;
+  right: 27%;
+}
+
+.pokemon__name {
+  color: #3a444d;
+  text-transform: capitalize;
 }
 </style>
